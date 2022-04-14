a28e2f53e48ec03abdc8b358cc336b10a6c4f3ab
Lucas Caldeira <mrrealcaldeira@gmail.com>
2022-04-14 14:03:39 -0300

    criação do header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import React from 'react';
 import './App.css';
+
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core';
+import purple from '@material-ui/core/colors/purple';
+
+import AppBar from '@material-ui/core/AppBar';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
 
 function App() {
   return (
+    <MuiThemeProvider theme={theme}>
+      <AppBar position="static" color='primary'>
+        <div className='header'>
+          <p className='title'>Lista de compras</p>
+        </div>
+      </AppBar>
     <div>
       <h1>Test</h1>
     </div>
+    </MuiThemeProvider>
   );
 }
 
