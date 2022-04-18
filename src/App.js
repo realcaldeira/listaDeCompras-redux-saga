2f38893b825795104c2e634ff3d175eab775073d
Lucas Caldeira <mrrealcaldeira@gmail.com>
2022-04-18 10:45:30 -0300

    Criando rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import { createMuiTheme } from '@material-ui/core/styles';
 import { MuiThemeProvider } from '@material-ui/core';
@@ -8,6 +8,7 @@ import purple from '@material-ui/core/colors/purple';
 
 import Header from './components/Header';
 import Home from './home';
+import CreateList from './createList';
 
 const theme = createMuiTheme({
   palette: {
@@ -21,9 +22,17 @@ const theme = createMuiTheme({
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
+      <Router>
+       <div>
       <Header />
+       <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/lista' component={CreateList} />
+        </Switch>
+       </div>
+      </Router>
+      
     
-      <Home />
     </MuiThemeProvider>
   );
 }
---
+++ src/createList/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CreateList(){
+  return(
+    <li>lista</li>
+  )
+}
