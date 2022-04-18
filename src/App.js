ad2a779e210013a8f4dd478b9b57aac1b7f9187b
Lucas Caldeira <mrrealcaldeira@gmail.com>
2022-04-18 17:15:23 -0300

    começando com o redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { createTheme } from '@material-ui/core/styles';
 import { MuiThemeProvider } from '@material-ui/core';
@@ -9,6 +10,7 @@ import purple from '@material-ui/core/colors/purple';
 import Header from './components/Header';
 import Home from './home';
 import CreateList from './createList';
+import store from './store';
 
 const theme = createTheme({
   palette: {
@@ -21,18 +23,20 @@ const theme = createTheme({
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Router>
-       <div>
-      <Header />
-       <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/lista' component={CreateList} />
-        </Switch>
-       </div>
-      </Router>
-      
-    </MuiThemeProvider>
+    <Provider store={store}> 
+      <MuiThemeProvider theme={theme}>
+        <Router>
+        <div>
+        <Header />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/lista' component={CreateList} />
+          </Switch>
+        </div>
+        </Router>
+        
+      </MuiThemeProvider>
+    </Provider>
   );
 }
 
